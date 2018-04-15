100c94807da8af0747a0eb3a1fa574a0c5feaa9a
Vishnu kn <vishnunagaraj7@gmail.com>
2018-04-15 16:27:58 +0530

    Added a create table command

--- connection.php
+++ connection.php
@@ -12,4 +12,17 @@ if($link->connect_errno){
 	die('Connection error: '. $link->connect_error);
 }
 
+$q = "
+		CREATE TABLE IF NOT EXISTS users
+		id int NOT NULL AUTO_INCREMENT,
+		email text NOT NULL,
+		password text NOT NULL,
+		diary longtext NOT NULL
+		PRIMARY KEY (id)
+	 ";
+$result = $link->query($q);
+if(!$result){
+	echo '<strong>Failed to create table</strong>';
+}
+
 ?>
